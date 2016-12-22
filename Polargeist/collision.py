e93c693350eb0288f0fdd6e995e30bedde5df963
nara888 <nara-888@hanmail.net>
2016-12-22 22:36:21 +0900

    final

---
+++ Polargeist/collision.py
@@ -0,0 +1,43 @@
+
+def Collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    return True
+
+def TableCollide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    if top_b - 18 < bottom_a < top_b:
+        return True
+
+def DownCollide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    if bottom_b + 18 > top_a > bottom_b:
+        return True
+
+def SideCollide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    if bottom_b + 18 > top_a > bottom_b:
+        return True
---
+++ Polargeist/end_line.py
@@ -0,0 +1,32 @@
+from pico2d import*
+
+#85초
+
+class EndLine:
+    PIXEL_PER_METER = (70.0 / 0.1)  # 10 pixel 30cm
+    RUN_SPEED_KMPH = 3.4  # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)  # mpm = 1분에 몇미터
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)  # MPS = 1초당 몇미터
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)  # PPS = pulse per second(?)
+    #  스피드 인듯
+
+    image = None
+
+    def __init__(self):
+        self.x = 56500
+        self.total_frame = 0.0
+        if EndLine.image == None:
+            EndLine.image = load_image('Ground\\end.png')
+
+    def update(self, frame_time):
+        distance = EndLine.RUN_SPEED_PPS * frame_time
+        self.total_frame += frame_time
+
+        if 85 > self.total_frame >= 1.1:
+                self.x -= distance
+
+    def stop(self):
+        self.dead = True
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 200, 1000, self.x, 495, 130, 650)
